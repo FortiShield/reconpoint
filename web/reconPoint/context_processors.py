0c9c2dc83887576a9228605877526eef0fd9dbf2
FortiShield <FortiShield@users.noreply.github.com>
2026-01-19 00:22:27 +0000

    deploy: 85f31529a716dd644b3bdc8ed4bf77007c9fa1e8

---
+++ web/reconPoint/context_processors.py
@@ -0,0 +1,23 @@
+from django.conf import settings
+
+from dashboard.models import *
+
+
+def projects(request):
+    projects = Project.objects.all()
+    try:
+        slug = request.resolver_match.kwargs.get("slug")
+        project = Project.objects.get(slug=slug)
+    except Exception:
+        project = None
+    return {"projects": projects, "current_project": project}
+
+
+def version_context(request):
+    return {"RECONPOINT_CURRENT_VERSION": settings.RECONPOINT_CURRENT_VERSION}
+
+
+def user_preferences(request):
+    if hasattr(request, "user_preferences"):
+        return {"user_preferences": request.user_preferences}
+    return {}
